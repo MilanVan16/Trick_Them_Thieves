4e9daae834df62b077c1c2abcaed6bd1bea6a391
Milan <milan.vanhaeverbeke@outlook.com>
2025-04-25 09:27:27 +0200

    Variables
    New needed variables in general_variables

--- Assets/Scripts/General_Game.cs
+++ Assets/Scripts/General_Game.cs
@@ -10,7 +10,11 @@ public static class General_Game : object
     public static int CurrentDoneObjectives = 0;
 
     [HideInInspector]
+    public static int ObjectivesCount = 5;
+
+    [HideInInspector]
     public static float ObjectiveRadius = 2;
 
-    
+    [HideInInspector]
+    public static bool IsHidden;
 }
