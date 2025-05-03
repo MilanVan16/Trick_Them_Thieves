5f1e405250a53c2ef7aebf054a0d011c61998f94
Milan <milan.vanhaeverbeke@outlook.com>
2025-05-03 12:18:36 +0200

    Updated PathFinding + implemented map

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,34 +4,65 @@ using UnityEngine.AI;
 
 public class EnemyPatrol : MonoBehaviour
 {
-    public Transform[] waypoints; // Drag Waypoints into inspector if we make a longer path
-    private NavMeshAgent agent;  // NavMesh and NavAgent in place so it doesnt walk through walls
-    private int currentWaypointIndex = 0;
+    #region Enemy pathing
+    [SerializeField]
+    private Transform[] _waypointCollectionPerObjective; // Drag Waypoints into inspector if we make a longer path
+
+    private Transform[][] _waypointsPerObjective;
+
+
+    private NavMeshAgent _agent;  // NavMesh and NavAgent in place so it doesnt walk through walls
+
+
+    private int _currentObjectiveWaypointCollection = 0;
+    private int _currentWaypoint = 0;
+    #endregion
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        _agent = GetComponent<NavMeshAgent>();
 
-        if (waypoints.Length > 0)
+        _waypointsPerObjective = new Transform[General_Game.ObjectivesCount][];
+
+        for (int i = 0; i <= _waypointCollectionPerObjective.Length - 1; i++)
         {
-            agent.SetDestination(waypoints[currentWaypointIndex].position);
+            int amountOfWaypoints = _waypointCollectionPerObjective[i].transform.childCount;
+
+            _waypointsPerObjective[i] = new Transform[amountOfWaypoints];
+
+            for (int j = 0; j <= amountOfWaypoints - 1; j++)
+            {
+                _waypointsPerObjective[i][j] = _waypointCollectionPerObjective[i].transform.GetChild(j);
+            }
+
         }
+
+        _agent.SetDestination(_waypointsPerObjective[General_Game.CurrentDoneObjectives][0].position);
+        _agent.updateRotation = true;
     }
 
     void Update()
     {
-        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+        if (_agent.remainingDistance <= _agent.stoppingDistance && !_agent.pathPending)
         {
             // Go to the next waypoint
-            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length; //% icon makes it loop back to first waypoint
-            agent.SetDestination(waypoints[currentWaypointIndex].position);
+            _currentWaypoint = (_currentWaypoint +1)% _waypointsPerObjective[_currentObjectiveWaypointCollection].Length;
+
+            _agent.SetDestination(_waypointsPerObjective[_currentObjectiveWaypointCollection][_currentWaypoint].position);
+        }
+
+        if(_currentObjectiveWaypointCollection != General_Game.CurrentDoneObjectives)
+        {
+            _currentObjectiveWaypointCollection = General_Game.CurrentDoneObjectives;
+            _agent.ResetPath();
+            _currentWaypoint = 0;
         }
     }
     private void OnTriggerEnter(Collider other)   // I tried using OnCollision but couldn't get it to work but on trigger works
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Enemy touched the Player (Trigger)! Game Over!"); 
+            Debug.Log("Enemy touched the Player (Trigger)! Game Over!");
             Time.timeScale = 0f; // Freeze the game
         }
     }
